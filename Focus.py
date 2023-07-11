45b8982150286a05f069ffaed2e9f623ac87eab5
GeorgeZhang184 <89557663+GeorgeZhang184@users.noreply.github.com>
2023-07-11 10:47:28 +0800

    Update Focus.py

--- Focus.py
+++ Focus.py
@@ -1,6 +1,22 @@
 import time
+from tkinter import *
+window=Tk()
+window.title('Focus Timer')
+window.geometry("350x200")
+lbl = Label(window, text="Start focusing!", font=("Arial Bold", 35))
+lbl.grid(column=10, row=0)
+def clicked():
+  tite = Label(window, text="How many minutes do you want to focus?", font=("Arial", 20))
+  spin = Spinbox(window, from_=1, to=240, width=10)
+  spin.grid(column=0, row=0)
+  focustime=spin.get()
+  start = Button(window, text="Start focusing!", command=clicked)
+  start.grid(column=2, row=0)
+
+btn = Button(window, text="Start!", bg='green', fg='white', command=clicked)
+btn.grid(column=10, row=10)
+window.mainloop()
 while True:
-  focustime=int(input("How long do you want to focus? Please enter minutes."))
   focussec=focustime*60
   while focussec:
     mins,secs = divmod(focussec,60)
